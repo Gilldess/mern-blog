64e677bd9173b63fb0e4907da7a4945afee87297
GillDess <agilldestaa23@gmail.com>
2025-08-11 09:38:56 +0700

    creat user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mogoose from "mongoose";
+
+const userSchema = new mogoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+},{ timestamps: true }
+);
+
+const User = mogoose.model('User', userSchema);
+
+export default User
